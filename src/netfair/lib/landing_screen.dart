63d5f557d3e3b80c0b297427b235cb5ed35db378
Joao <jrcp210@gmail.com>
2020-11-09 21:27:39 +0000

    Reorganized screens in different files, started routing

---
+++ src/netfair/lib/landing_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to first screen when tapped.
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
