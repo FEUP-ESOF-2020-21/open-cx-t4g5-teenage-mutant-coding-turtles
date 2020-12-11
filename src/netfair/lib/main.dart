3512cd5825ae222a1e07a119fb1a424c16564e6f
Joao <jrcp210@gmail.com>
2020-12-11 10:05:02 +0000

    Updated path to screens

--- src/netfair/lib/main.dart
+++ src/netfair/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'file:///C:/Workspace/Feup/ESOF/repo/src/netfair/lib/Screens/create_event_screen.dart';
-import 'file:///C:/Workspace/Feup/ESOF/repo/src/netfair/lib/Screens/landing_screen.dart';
-import 'file:///C:/Workspace/Feup/ESOF/repo/src/netfair/lib/Screens/login_screen.dart';
-import 'file:///C:/Workspace/Feup/ESOF/repo/src/netfair/lib/Screens/profile_screen.dart';
-import 'file:///C:/Workspace/Feup/ESOF/repo/src/netfair/lib/Screens/sign_up_screen.dart';
-import 'file:///C:/Workspace/Feup/ESOF/repo/src/netfair/lib/Screens/event_profile_screen.dart';
-import 'file:///C:/Workspace/Feup/ESOF/repo/src/netfair/lib/Screens/create_profile_screen.dart';
-import 'file:///C:/Workspace/Feup/ESOF/repo/src/netfair/lib/Screens/choose_user_type_screen.dart';
+import 'Screens/choose_user_type_screen.dart';
+import 'Screens/create_event_screen.dart';
+import 'Screens/create_profile_screen.dart';
+import 'Screens/event_profile_screen.dart';
+import 'Screens/landing_screen.dart';
+import 'Screens/login_screen.dart';
+import 'Screens/profile_screen.dart';
+import 'Screens/sign_up_screen.dart';
+
 
 
 void main() {
