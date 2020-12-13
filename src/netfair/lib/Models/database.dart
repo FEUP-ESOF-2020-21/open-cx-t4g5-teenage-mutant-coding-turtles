9bc25d1b8c0aa6248a1cacb468785ea0da6093bf
Joao <jrcp210@gmail.com>
2020-12-13 14:33:14 +0000

    Added event db connection and model

--- src/netfair/lib/Models/database.dart
+++ src/netfair/lib/Models/database.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:netfair/Models/event.dart';
 import 'user.dart';
 
 final databaseReference = FirebaseDatabase.instance.reference();
@@ -8,5 +9,11 @@ final FirebaseAuth _auth = FirebaseAuth.instance;
 DatabaseReference saveUser(DBUser user) {
   var id = databaseReference.child('users/'+ _auth.currentUser.uid);
   id.set(user.toJson());
+  return id;
+}
+
+DatabaseReference saveEvent(DBEvent event) {
+  var id = databaseReference.child('events/').push();
+  id.set(event.toJson());
   return id;
 }
